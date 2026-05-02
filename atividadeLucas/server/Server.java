b10a415e6530b246e90a3d44ee223ada1e2f1641
Daniel Silva <147885397+danieldsilvadev@users.noreply.github.com>
2026-05-02 09:52:30 -0300

    Delete atividadeLucas directory

--- atividadeLucas/server/Server.java
+++
@@ -1,8 +0,0 @@
-package atividadeLucas.server;
-
-public class Server {
-    public static void main(String[] args) {
-        ServerConfig servidor = new ServerConfig();
-        servidor.startServer(8089);
-    }
-}
