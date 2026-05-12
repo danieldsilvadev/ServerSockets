f28ee73605d27c3b23382b06e8bd4c9f80c6a556
Daniel Silva <147885397+danieldsilvadev@users.noreply.github.com>
2026-05-12 11:19:16 -0300

    Delete atividadeLucas directory

--- atividadeLucas/client/ClienteOne.java
+++
@@ -1,8 +0,0 @@
-package atividadeLucas.client;
-
-public class ClienteOne {
-    public static void main(String[] args) {
-        ClienteConfig clienteOne = new ClienteConfig();
-        clienteOne.abrirConexao("localhost",  8089);
-    }
-}
--- atividadeLucas/client/ClienteTwo.java
+++
@@ -1,9 +0,0 @@
-package atividadeLucas.client;
-
-public class ClienteTwo {
-    public static void main(String[] args) {
-        ClienteConfig clienteTwo = new ClienteConfig();
-        clienteTwo.abrirConexao("192.168.56.1",  8089);
-      
-    }
-}
